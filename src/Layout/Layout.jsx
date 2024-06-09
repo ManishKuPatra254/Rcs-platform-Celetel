261170449bcb07efc2260a1f8e0e3b33b523b1c0
ManishKuPatra254 <manishpatra254@gmail.com>
2024-06-09 21:23:44 +0530

    created templates folder properly

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -324,7 +324,7 @@ const UserLayout = ({ children }) => {
     return (
         <Fragment>
             <div className="flex min-h-screen w-full flex-col bg-muted/40">
-                {/* this is the sidebar content  */}
+
                 <aside className="fixed inset-y-0 left-0 z-10 hidden w-16 flex-col border-r bg-background sm:flex">
                     <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
                         <Link
@@ -352,7 +352,7 @@ const UserLayout = ({ children }) => {
                             <TooltipTrigger asChild>
                                 <Link
                                     to={'/sendrcs'}
-                                    className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                                 >
                                     <Send className="h-5 w-5" />
                                     <span className="sr-only">Send Rcs</span>
--- src/User/Chatsinfo.jsx
+++ src/User/Chatsinfo.jsx
@@ -1,91 +1,240 @@
-import { Layout } from '@/Layout/Layout'
-import { Fragment } from 'react'
-import { Button } from "@/components/ui/button"
+import { Layout } from '@/Layout/Layout';
+import { Fragment, useState } from 'react';
 import {
     Card,
     CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+
 import {
     Tabs,
     TabsContent,
     TabsList,
     TabsTrigger,
-} from "@/components/ui/tabs"
+} from "@/components/ui/tabs";
+import { Input } from '@/components/ui/input';
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { ArchiveRestore, Clock, CornerDownLeft, Mic, Package2, Paperclip, Trash2, Undo2 } from 'lucide-react';
+import { Label } from '@radix-ui/react-label';
+import { Link } from 'react-router-dom';
 
 export default function Chatdetails() {
+    const [selectedCard, setSelectedCard] = useState(null);
+
+    const handleCardClick = (cardDetails) => {
+        setSelectedCard(cardDetails);
+    };
+
+    const cardDetailsList = [
+        {
+            title: 'William Smith',
+            description: 'Meeting Tomorrow',
+        },
+        {
+            title: 'Alice Smith',
+            description: 'Project Update',
+
+        },
+        {
+            title: 'Bob Johnson',
+            description: 'Weekend Plans',
+
+        },
+        {
+            title: 'Emily Davis',
+            description: 'Question about Budget Update',
+
+        },
+        {
+            title: 'Michael Wilson',
+            description: 'Important Announcement',
+
+        },
+        {
+            title: 'Sarah Brown',
+            description: 'Feedback on Proposal',
+        },
+        {
+            title: 'David Lee',
+            description: 'New Project Idea',
+
+        },
+        {
+            title: 'Daniel Johnson',
+            description: 'Feedback Request',
+
+        },
+    ];
+
     return (
         <Fragment>
             <Layout>
-                <div className="grid grid-cols-2 gap-2 border-2 p-2 md:grid-cols-1 sm:grid-cols-1 lg:grid-cols-2">
-                    <div className="border-2 p-4">
-                        <div className="">
-                            <h2 className='font-bold'>Chats</h2>
-                        </div>
+                <div className="grid grid-cols-2 gap-2 p-2 md:grid-cols-1 sm:grid-cols-1">
+                    <ResizablePanelGroup
+                        direction="horizontal"
+                        className="max-w-full rounded-lg border"
+                    >
+                        <ResizablePanel defaultSize={40}>
+                            <div className="rounded-md p-3">
+                                <div className="flex justify-between">
+                                    <Tabs defaultValue="cURL" className="w-full">
+                                        <div className="p-4 flex justify-between items-center w-full">
+                                            <h2 className='font-bold text-3xl'>Chats</h2>
+                                            <TabsList>
+                                                <TabsTrigger value="cURL">All chats</TabsTrigger>
+                                                <TabsTrigger value="PHP">Unread</TabsTrigger>
+                                            </TabsList>
+                                        </div>
+                                        <div className="p-4">
+                                            <Input
+                                                placeholder="Search"
+                                                className="w-full text-sm"
+                                            />
+                                        </div>
+                                        <TabsContent value="cURL">
+                                            <div className="h-[600px] overflow-auto custom-scrollbar p-3">
+                                                {cardDetailsList.map((cardDetails, index) => (
+                                                    <div key={index} onClick={() => handleCardClick(cardDetails)} className="mb-2 cursor-pointer">
+                                                        <div className="px-4 border rounded-sm py-3">
+                                                            <CardTitle className="text-sm font-bold">{cardDetails.title}</CardTitle>
+                                                            <p className="text-sm">{cardDetails.description}</p>
+                                                            <CardDescription className="text-xs font-light truncate">
+                                                                {cardDetails.content}
+                                                            </CardDescription>
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </TabsContent>
+                                        <TabsContent value="PHP">
+                                            <Card x-chunk="dashboard-05-chunk-3" className="mb-4">
+                                                <CardHeader className="px-7">
+                                                    <CardTitle>Example API Request</CardTitle>
+                                                    <CardDescription>
+                                                        Determine the URL of the API endpoint you need to interact with. This is provided by the API documentation.
+                                                    </CardDescription>
+                                                </CardHeader>
+                                                <CardContent>
+                                                    <div className="overflow-x-auto">
+                                                        <pre className="whitespace-pre-wrap text-sm"></pre>
+                                                    </div>
+                                                </CardContent>
+                                            </Card>
+                                        </TabsContent>
+                                    </Tabs>
+                                </div>
+                            </div>
+                        </ResizablePanel>
+                        <ResizableHandle withHandle />
+                        <ResizablePanel defaultSize={60}>
+                            <div className="p-4 w-full flex flex-col h-full">
+                                <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-transparent">
+                                    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+                                        <Link
+                                            href="#"
+                                            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+                                        >
+                                            <Package2 className="h-5 w-5" />
+                                        </Link>
+                                        <Link
+                                            href="#"
+                                            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+                                        >
+                                            <ArchiveRestore className="h-5 w-5" />
 
-                        <Tabs defaultValue="account" className="w-[400px]">
-                            <TabsList className="grid w-full grid-cols-2">
-                                <TabsTrigger value="account">Account</TabsTrigger>
-                                <TabsTrigger value="password">Password</TabsTrigger>
-                            </TabsList>
-                            <TabsContent value="account">
-                                <Card>
-                                    <CardHeader>
-                                        <CardTitle>Account</CardTitle>
-                                        <CardDescription>
-                                            Make changes to your account here. Click save when youre done.
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <CardContent className="space-y-2">
-                                        <div className="space-y-1">
-                                            <Label htmlFor="name">Name</Label>
-                                            <Input id="name" defaultValue="Pedro Duarte" />
+                                        </Link>
+                                        <Link
+                                            href="#"
+                                            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+                                        >
+                                            <Trash2 className="h-5 w-5" />
+                                        </Link>
+                                        <Link
+                                            href="#"
+                                            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+                                        >
+                                            <Clock className="h-6 w-6" />
+                                        </Link>
+
+                                    </nav>
+
+                                    <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+                                        <div className="">
+                                            <Undo2 />
+
                                         </div>
-                                        <div className="space-y-1">
-                                            <Label htmlFor="username">Username</Label>
-                                            <Input id="username" defaultValue="@peduarte" />
+                                    </div>
+                                </header>
+                                {selectedCard ? (
+                                    <div>
+                                        <Avatar>
+                                            <AvatarFallback>CN</AvatarFallback>
+                                        </Avatar>
+                                        <h3 className="text-sm font-bold">{selectedCard.title}</h3>
+                                        <p className="text-xs">{selectedCard.description}</p>
+                                        <div className="border mt-4"></div>
+                                        <p className='text-xs mt-3'>{selectedCard.content}</p>
+                                    </div>
+                                ) : (
+                                    <p className='text-xs mt-3'>Select a card to see details here</p>
+                                )}
+
+                                <div className="flex-grow"></div>
+                                <div className="border mb-8"></div>
+                                <div className="sticky bottom-0">
+                                    <form
+                                        // eslint-disable-next-line react/no-unknown-property
+                                        className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring" x-chunk="dashboard-03-chunk-1"
+                                    >
+                                        <Label htmlFor="message" className="sr-only">
+                                            Message
+                                        </Label>
+                                        <Textarea
+                                            id="message"
+                                            placeholder="Type your message here..."
+                                            className="min-h-12 resize-none border-0 p-3 outline-none shadow-none focus-visible:ring-0"
+                                        />
+                                        <div className="flex items-center p-3 pt-0 mt-2">
+                                            <Tooltip>
+                                                <TooltipTrigger asChild>
+                                                    <Button variant="ghost" size="icon">
+                                                        <Paperclip className="size-4" />
+                                                        <span className="sr-only">Attach file</span>
+                                                    </Button>
+                                                </TooltipTrigger>
+                                                <TooltipContent side="top">Attach File</TooltipContent>
+                                            </Tooltip>
+                                            <Tooltip>
+                                                <TooltipTrigger asChild>
+                                                    <Button variant="ghost" size="icon">
+                                                        <Mic className="size-4" />
+                                                        <span className="sr-only">Use Microphone</span>
+                                                    </Button>
+                                                </TooltipTrigger>
+                                                <TooltipContent side="top">Use Microphone</TooltipContent>
+                                            </Tooltip>
+                                            <Button size="sm" className="ml-auto gap-1.5">
+                                                Send Message
+                                                <CornerDownLeft className="size-3.5" />
+                                            </Button>
                                         </div>
-                                    </CardContent>
-                                    <CardFooter>
-                                        <Button>Save changes</Button>
-                                    </CardFooter>
-                                </Card>
-                            </TabsContent>
-                            <TabsContent value="password">
-                                <Card>
-                                    <CardHeader>
-                                        <CardTitle>Password</CardTitle>
-                                        <CardDescription>
-                                            Change your password here. After saving, youll be logged out.
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <CardContent className="space-y-2">
-                                        <div className="space-y-1">
-                                            <Label htmlFor="current">Current password</Label>
-                                            <Input id="current" type="password" />
-                                        </div>
-                                        <div className="space-y-1">
-                                            <Label htmlFor="new">New password</Label>
-                                            <Input id="new" type="password" />
-                                        </div>
-                                    </CardContent>
-                                    <CardFooter>
-                                        <Button>Save password</Button>
-                                    </CardFooter>
-                                </Card>
-                            </TabsContent>
-                        </Tabs>
-                    </div>
-                    <div className="border-2 p-4 ">
-                        dkfjhjhgjfhgjf
-                    </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </ResizablePanel>
+                    </ResizablePanelGroup>
                 </div>
             </Layout>
         </Fragment>
-    )
+    );
 }
